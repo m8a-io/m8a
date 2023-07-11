da3631758f65b31a4b87d703f928a00a5098050e
Scott Molinari <scott.molinari@adduco.de>
2023-07-11 09:53:19 +0200

    fix(starter-kit-app): fix linting issues

--- packages/logger/src/logger/logger.module.ts
+++ packages/logger/src/logger/logger.module.ts
@@ -1,9 +1,10 @@
+import { FastifyParser } from '@ogma/platform-fastify'
 import { Module } from '@nestjs/common'
 import { DevLoggerService } from './dev-logger.service'
 import { ProdLoggerService } from './prod-logger.service'
 
 @Module({
-  providers: [DevLoggerService, ProdLoggerService],
+  providers: [DevLoggerService, ProdLoggerService, FastifyParser],
   exports: [DevLoggerService, ProdLoggerService]
 })
 export class LoggerModule {}
