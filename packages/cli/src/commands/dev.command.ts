a94ee9a7cac45cd9a5cbc3046491e59b1640acb8
Scott Molinari <scott.molinari@adduco.de>
2023-07-28 11:51:10 +0000

    feat(cli): add m8a dev commands

--- packages/cli/src/commands/dev.command.ts
+++ packages/cli/src/commands/dev.command.ts
@@ -1,11 +1,12 @@
-import { Command, CommandRunner } from 'nest-commander'
+import { Command, CommandRunner, Option } from 'nest-commander'
 import { CheckForService } from '../utils/checkFor.service'
 import { DevLoggerService } from '@m8a/logger'
 import { RunnerService } from '../utils/runner.service'
 
 @Command({
   name: 'dev',
-  description: 'The "dev" command will start a local development server.'
+  description:
+    'The "dev" command (with one of the app types option) will start a local development server for that app type (i.e. "app" or "api" or "cli").'
 })
 export class DevCommand extends CommandRunner {
   constructor (
@@ -17,14 +18,43 @@ export class DevCommand extends CommandRunner {
   }
 
   async run (passedParams: string[]): Promise<void> {
-    this.runApiDev(passedParams)
+    if (passedParams.length > 1) {
+      this.logService.log('You can only give the "dev" command one option at a time.')
+    }
+    const appType = passedParams[0].toLowerCase()
+    if (appType === 'api') {
+      this.runAppDev(appType)
+    } else if (appType === 'app') {
+      this.runApiDev(appType)
+    } else {
+      this.logService.log(`You've given an unknown option to the "dev" command. ${appType}?`)
+      this.logService.log('Please try again!')
+    }
   }
 
-  private runApiDev (passedParams): void {
-    if (passedParams === 'api') {
-      this.logService.addLine()
-      this.logService.log('Starting your API dev environment....')
-      this.runnerService.spawnSync('pnpm', ['start:dev'])
-    }
+  @Option({
+    flags: 'api',
+    description: 'This will start the api (backend) dev server.'
+  })
+  private runApiDev (appType: string): void {
+    this.logService.addLine()
+    this.logService.log(`Moving to the ${appType} folder`)
+    process.chdir('/')
+    process.chdir(`home/dev/m8a/packages/starter-kit-${appType}`)
+    this.logService.log(`Starting your ${appType.toUpperCase()} dev environment....`)
+    this.runnerService.spawnSync('quasar', ['dev'])
+  }
+
+  @Option({
+    flags: 'app',
+    description: 'This will start the app (frontend) dev server.'
+  })
+  private runAppDev (appType: string): void {
+    this.logService.addLine()
+    this.logService.log(`Moving to the ${appType} folder`)
+    process.chdir('/')
+    process.chdir(`home/dev/m8a/packages/starter-kit-${appType}`)
+    this.logService.log(`Starting your ${appType.toUpperCase()} dev environment....`)
+    this.runnerService.spawnSync('quasar', ['dev'])
   }
 }
