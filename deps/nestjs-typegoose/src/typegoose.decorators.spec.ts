aa972f42dd7681df60f6396f70ba5117ddc592ff
Ben Neigher <benneigher@rivian.com>
2023-08-25 23:43:04 -0700

    feature(InjectConnection): Inject connection for session / transaction support

--- deps/nestjs-typegoose/src/typegoose.decorators.spec.ts
+++ deps/nestjs-typegoose/src/typegoose.decorators.spec.ts
@@ -1,29 +1,30 @@
-import { prop } from '@typegoose/typegoose'
-import * as nest from '@nestjs/common'
-import { InjectModel, InjectConnection } from './typegoose.decorators'
-import { DEFAULT_DB_CONNECTION_NAME } from './typegoose.constants'
+import { prop } from "@typegoose/typegoose";
+import * as nest from "@nestjs/common";
+import { InjectModel, InjectConnection } from "./typegoose.decorators";
+import { DEFAULT_DB_CONNECTION_NAME } from "./typegoose.constants";
 
-jest.mock('@nestjs/common', () => ({
-  Inject: jest.fn()
-}))
+jest.mock("@nestjs/common", () => ({
+  Inject: jest.fn(),
+}));
 
 class MockUser {
   @prop()
-  name: string
+  name: string;
 }
 
-describe('InjectModel', () => {
-  it('should inject the model', () => {
-    InjectModel(MockUser)
+describe("InjectModel", () => {
+  it("should inject the model", () => {
+    InjectModel(MockUser);
 
-    expect(nest.Inject).toHaveBeenCalledWith('MockUserModel')
-  })
-})
+    expect(nest.Inject).toHaveBeenCalledWith("MockUserModel");
+  });
+});
 
-describe('InjectConnection', () => {
-  it('should inject the connection', () => {
-    InjectConnection()
 
-    expect(nest.Inject).toHaveBeenCalledWith(DEFAULT_DB_CONNECTION_NAME)
-  })
-})
+describe("InjectConnection", () => {
+  it("should inject the connection", () => {
+    InjectConnection();
+
+    expect(nest.Inject).toHaveBeenCalledWith(DEFAULT_DB_CONNECTION_NAME);
+  });
+});
