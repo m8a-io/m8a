2f2ebf8a1a6cc65cd186bd3b0ce675315ad90f91
zeus-dev <smolinari>
2023-09-27 06:15:11 +0000

    chore(cli): bump version and add small fix

--- libs/logger/src/logger/logger.module.ts
+++ libs/logger/src/logger/logger.module.ts
@@ -1,10 +1,9 @@
-import { FastifyParser } from '@ogma/platform-fastify'
 import { Module } from '@nestjs/common'
 import { DevLoggerService } from './dev-logger.service'
 import { ProdLoggerService } from './prod-logger.service'
 
 @Module({
-  providers: [DevLoggerService, ProdLoggerService, FastifyParser],
+  providers: [DevLoggerService, ProdLoggerService],
   exports: [DevLoggerService, ProdLoggerService]
 })
 export class LoggerModule {}
